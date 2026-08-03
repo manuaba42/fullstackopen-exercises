53ca9bfdf27e0efcdc67a480670f860c0936dfd8
manuaba42 <ssedanayoga3@gmail.com>
2026-08-03 09:58:47 +0800

    Blog List, step 2 refactor

---
+++ part4/blog_list/controllers/blogs.js
@@ -0,0 +1,68 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blogs')
+
+blogsRouter.get('/', (request, response) => {
+  Blog.find({}).then(blogs => {
+    response.json(blogs)
+  })
+})
+
+// blogsRouter.get('/:id', (request, response, next) => {
+//   Blog.findById(request.params.id)
+//     .then(blog => {
+//       if (blog) {
+//         response.json(blog)
+//       } else {
+//         response.status(404).end()
+//       }
+//     })
+//     .catch(error => next(error))
+// })
+
+blogsRouter.post('/', (request, response, next) => {
+  const body = request.body
+
+  const newBlog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+  })
+
+  newBlog.save()
+    .then(savedBlog => {
+      response.json(savedBlog)
+    })
+    .catch(error => next(error))
+})
+
+// blogsRouter.delete('/:id', (request, response, next) => {
+//   Blog.findByIdAndDelete(request.params.id)
+//     .then(() => {
+//       response.status(204).end()
+//     })
+//     .catch(error => next(error))
+// })
+
+// blogsRouter.put('/:id', (request, response, next) => {
+//   const { title, author, url, likes  } = request.body
+
+//   Blog.findById(request.params.id)
+//     .then(selectedBlog => {
+//       if (!selectedBlog) {
+//         return response.status(404).end()
+//       }
+
+//       selectedBlog.title = title
+//       selectedBlog.author = author
+//       selectedBlog.url = url
+//       selectedBlog.likes = likes
+
+//       return selectedBlog.save().then((updatedBlog) => {
+//         response.json(updatedBlog)
+//       })
+//     })
+//     .catch(error => next(error))
+// })
+
+module.exports = blogsRouter
--- part4/blog_list/index.js
+++ part4/blog_list/index.js
@@ -1,41 +1,49 @@
-const express = require('express')
-const mongoose = require('mongoose')
+// const express = require('express')
+// const mongoose = require('mongoose')
 
-const app = express()
+// const app = express()
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
+// const blogSchema = mongoose.Schema({
+//   title: String,
+//   author: String,
+//   url: String,
+//   likes: Number,
+// })
 
-const password = process.argv[2]
+// const password = process.argv[2]
 
-// console.log('password', process.argv)
+// // console.log('password', process.argv)
 
-const Blog = mongoose.model('Blog', blogSchema)
+// const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = 'mongodb+srv://manuaba:'+password+'@cluster0.nxcyat3.mongodb.net/?appName=Cluster0'
-mongoose.connect(mongoUrl, { family: 4 })
+// const mongoUrl = 'mongodb+srv://manuaba:'+password+'@cluster0.nxcyat3.mongodb.net/?appName=Cluster0'
+// mongoose.connect(mongoUrl, { family: 4 })
 
-app.use(express.json())
+// app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+// app.get('/api/blogs', (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs)
+//   })
+// })
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+// app.post('/api/blogs', (request, response) => {
+//   const blog = new Blog(request.body)
 
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+//   blog.save().then((result) => {
+//     response.status(201).json(result)
+//   })
+// })
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+// const PORT = 3003
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`)
+// })
+
+const app = require('./app') // the actual Express application
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
