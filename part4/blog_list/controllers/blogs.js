39671626ab9197cdd4bb74050dad7ec46d2e6673
manuaba42 <ssedanayoga3@gmail.com>
2026-08-31 12:27:26 +0800

    4.17: Blog List Expansion, step 5

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,11 +1,12 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
   // Blog.find({}).then(blogs => {
   //   response.json(blogs)
   // })
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -31,6 +32,12 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
 
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(400).json({ error: 'user not found' })
+  }
+
   // const newBlog = new Blog({
   //   title: body.title,
   //   author: body.author,
@@ -50,9 +57,13 @@ blogsRouter.post('/', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
+    user: user._id
   })
 
   const savedBlog = await newBlog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+
   response.status(201).json(savedBlog)
 })
 
